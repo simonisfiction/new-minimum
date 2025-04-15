69087ea8ad4a4c4c777a768e04e9d65e627bd93c
simonisfiction <simon.chesney@yahoo.com>
2025-04-14 17:43:47 -0700

    add a form to finance page and get the values from the form with javascript

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-document.querySelector("h1").innerHTML = "GoodBye";
+let form = document.forms["my-form"];
+form.addEventListener("submit", getValues);
+console.log("\n this is the beginning of the js file");
+function getValues(event){
+  event.preventDefault();
+
+  let formData = {
+    "income": form.income.value,
+    "rent": form.rent.value
+  }
+  let profit = parseFloat(formData.income) - parseFloat(formData.rent);
+  console.log(profit);
+  let out = `<p>Money left over: ${profit.toFixed(2)}</p>`;
+
+  document.querySelector(".out").innerHTML = out;
+}
