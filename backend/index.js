25fc220732a6b2d9ca02796dc3926bf391aecc86
simonisfiction <simon.chesney@yahoo.com>
2025-04-27 17:07:52 -0700

    update the server to route and serve the proper files

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 import express from "express";
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.resolve(__dirname + "/../frontend/")));
+
 app.get('/', (req, res) => {
-  console.log(req.headersDistinct);
-  res.send("Hello world");
-})
-
-
-
+  console.log(req.headers);
+  res.sendFile(path.resolve(__dirname + "/../frontend/finance.html"));
+});
 
 
 
